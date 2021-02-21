b4a6af00ad50256f2287f28b50b6bbbe518a10ef
dmp-devops <seubpong.mon@truedigital.group>
2021-02-21 22:55:28 +0700

    Executable!

---
+++ iasc-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iasc_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
