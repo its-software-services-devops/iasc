b4859eb9e1e9d10a499feaa21e3053b79065c9f7
dmp-devops <seubpong.mon@truedigital.group>
2021-02-24 21:44:25 +0700

    Added verbose option

--- iasc-app/Its/Iasc/Options/InitOptions.cs
+++ iasc-app/Its/Iasc/Options/InitOptions.cs
@@ -5,6 +5,8 @@ namespace Its.Iasc.Options
     [Verb("init", HelpText = " Initialize a new or existing Terraform working directory.")]
     public class InitOptions
     {
+        [Option('v', "verbose", Required = true, HelpText = "Set output to verbose messages.")]
+        public string Verbose { get; set; }
     }    
 }
 
--- iasc-app/Program.cs
+++ iasc-app/Program.cs
@@ -1,13 +1,20 @@
-﻿using Its.Iasc.Options;
+﻿using System;
 using CommandLine;
+using Its.Iasc.Options;
 
 namespace its.iasc
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void RunInit(InitOptions o)
         {
-            var result = Parser.Default.ParseArguments<InitOptions, PlanOptions, ApplyOptions>(args);
+            Console.WriteLine("Verbose = [{0}]", o.Verbose);
+        }
+
+        public static void Main(string[] args)
+        {
+            var result = Parser.Default.ParseArguments<InitOptions, PlanOptions, ApplyOptions>(args)
+                .WithParsed<InitOptions>(RunInit);
         }
     }
 }
