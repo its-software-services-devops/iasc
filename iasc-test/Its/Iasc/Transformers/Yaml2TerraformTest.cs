088b26c9a9ffbe0e46345a02384b99da7d9e2cd9
Seubpong Monsar <pjame.fb@gmail.com>
2021-04-28 20:53:31 +0700

    Added test

---
+++ iasc-test/Its/Iasc/Transformers/Yaml2TerraformTest.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using System.IO;
+using System.Collections.Generic;
+using Its.Iasc.Workflows;
+using Its.Iasc.Workflows.Models;
+
+namespace Its.Iasc.Transformers
+{
+    public class Yaml2TerraformTest
+    {
+        private string yaml1 = @"
+---
+02_gce_rke.tf: |       
+  config:
+    defaultChartId: helm-terraform-gcp
+
+  charts:
+    helm-terraform-gcp:
+      chartUrl: https://its-software-services-devops.github.io/helm-terraform-gcp/
+      version: 1.1.5-rc8
+
+  infraIasc:
+    - valuesFile: iasc-its-global.yaml
+      chartId: helm-terraform-gcp
+      version: 1.1.5-rc8
+      alias: iasc-its-global
+      chartUrl: https://its-software-services-devops.github.io/helm-terraform-gcp/
+
+    - valuesFile: iasc-its-gce-manager.yaml
+    - valuesFile: iasc-its-gce-rke.yaml
+";
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("")]
+        [TestCase("temp")]
+        public void TransformTestWithoutDir(string wipDir)
+        {
+            //Only one "02_gce_rke.tf: |" per block
+
+            var ctx = new Context();
+
+            ctx.WipDir = wipDir;
+            if (!string.IsNullOrEmpty(wipDir))
+            {
+                ctx.WipDir = Path.GetTempPath();
+            }
+            
+            var cfg = new Infra();
+            var tfm = new Yaml2Terraform(ctx);
+
+            var lines = new List<string>();
+            lines.Add(yaml1);
+
+            tfm.Transform(lines, cfg);
+        }
+    }
+}
